7031073ac19183310e8d6551d067f5cb1b27cfa7
martin0123478 <martin_agui97@outlook.es>
2021-12-13 13:30:09 -0600

    creacion de modulos, componentes, lazy loading

---
+++ src/app/mapas/mapas-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FullScreenComponent } from './pages/full-screen/full-screen.component';
+import { MarcadoresComponent } from './pages/marcadores/marcadores.component';
+import { PropiedadesComponent } from './pages/propiedades/propiedades.component';
+import { ZoomRangeComponent } from './pages/zoom-range/zoom-range.component';
+
+const routes: Routes = [
+  
+  {
+    path:'',
+    children:[
+      {
+        path:'fullScreen',component:FullScreenComponent
+      },
+      {
+        path:'zoomRange',component:ZoomRangeComponent
+      },
+      {
+        path:'marcadores',component:MarcadoresComponent
+      },
+      {
+        path:'propiedades',component:PropiedadesComponent
+      },
+      {
+        path:'**',redirectTo:'fullScreen'
+      }
+    ]
+    }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MapasRoutingModule { }
