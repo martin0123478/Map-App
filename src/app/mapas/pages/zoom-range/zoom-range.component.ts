54a616b306d1fa84eff76954fa84c9ce1b0bfd7c
martin0123478 <martin_agui97@outlook.es>
2021-12-14 12:30:03 -0600

    agregando la funcionalidad de ZoomIn y ZoomOut

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
-  ]
-})
-export class ZoomRangeComponent implements OnInit {
+    `
+    .mapa-container{
+      width:100%;
+      height:100%;
+    }
 
+    .row{
+      background-color:white;
+      bottom:50px;
+      z-index:999;
+      left:50px;
+      padding:10px;
+      position:fixed;
+    }
+    `
+  ]
+
+})
+export class ZoomRangeComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef
+mapa!: mapboxgl.Map;
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+        center:[ -99.29426584506004,19.60728573874393],
+        zoom:18
+      });
+  }
+
+  zoomOut(){
+    this.mapa.zoomOut();
+  }
+
+  zoomIn(){
+    this.mapa.zoomIn();
   }
 
 }
