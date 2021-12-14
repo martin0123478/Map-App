d33259868bbd26ed7b0f1eeeedf9f417516f98c3
martin0123478 <martin_agui97@outlook.es>
2021-12-13 22:18:50 -0600

    centrando y mejorando el zoom de la ubicación del mapa

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl'
-import { environment } from '../../../../environments/environment';
+
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
@@ -18,10 +18,12 @@ export class FullScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    (mapboxgl as any).accessToken = environment.mapboxToken;
+    
 var map = new mapboxgl.Map({
 container: 'mapa',
-style: 'mapbox://styles/mapbox/streets-v11'
+style: 'mapbox://styles/mapbox/streets-v11',
+  center:[ -99.29426584506004,19.60728573874393],
+  zoom:18
 });
   }
 
