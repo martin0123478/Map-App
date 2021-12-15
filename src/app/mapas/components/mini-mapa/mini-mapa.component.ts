0232ac90a199a4a744be8245114e992d80b0e332
martin0123478 <martin_agui97@outlook.es>
2021-12-14 22:13:12 -0600

    agregando posicion a propiedades

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
   styles: [
+    `
+    div{
+      wigth: 100%;
+      heigth:150px;
+      margin:0;
+    }
+    `
   ]
 })
-export class MiniMapaComponent implements OnInit {
-
+export class MiniMapaComponent implements AfterViewInit {
+ @Input() lnglat: [number,number] = [0,0];
+ @ViewChild('mapa') divMapa !:ElementRef;
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+        center:this.lnglat,
+        zoom:15
+      }); 
+      new mapboxgl.Marker()
+      .setLngLat(this.lnglat)
+      .addTo(map)
   }
 
 }
